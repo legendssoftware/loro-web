087b4d497074b5aa74b63796d4882bba935fb2f9
Brandon-Online01 <brandonnkawu01@gmail.com>
2025-02-21 09:22:10 +0200

    adds servcies suspension banner

--- components/ui/alert-banner.tsx
+++ components/ui/alert-banner.tsx
@@ -6,7 +6,7 @@ export function AlertBanner() {
             <div className="container px-2 py-2 mx-auto">
                 <div className="flex items-center justify-center gap-2 text-xs text-amber-50">
                     <AlertTriangle className="w-3 h-3" />
-                    <p className="text-sm font-normal">We are having issues with task creation. We will notify you once everything is in order.</p>
+                    <p className="text-sm font-normal">Services suspended for now. Please try again later.</p>
                 </div>
             </div>
         </div>
